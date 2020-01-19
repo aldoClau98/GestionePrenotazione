03f79bbecf8a7fa2d63cda6bdeba5a492ed27f28
Andrea <andreaclaro.04@gmail.com>
2020-01-19 11:33:10 +0100

    Introduced ajax for calendar

---
+++ Codice/GestionePrenotazioni/src/Controller/CalGetter.java
@@ -0,0 +1,38 @@
+package Controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+
+import Model.Struttura;
+import Model.StrutturaDAO;
+
+/**
+ * Author: Andrea Claro
+ */
+@WebServlet("/CalGetter")
+public class CalGetter extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String str = request.getParameter("data");
+		if (str != null) {
+			JSONArray prodJson = new JSONArray();
+			
+			response.setContentType("application/json");
+			response.getWriter().append(prodJson.toString());
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}

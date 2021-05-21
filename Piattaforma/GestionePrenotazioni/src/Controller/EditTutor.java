62a81cfdaccba01e3dd66d14cd831ff5d210145d
aldo <a.claudini3@gmail.com>
2021-05-21 10:27:33 +0200

    Controller:  fix bug

---
+++ Piattaforma/GestionePrenotazioni/src/Controller/EditTutor.java
@@ -0,0 +1,50 @@
+package Controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class EditTutor
+ */
+@WebServlet("/EditTutor")
+public class EditTutor extends ServletBasic {
+	private static final long serialVersionUID = 1L;
+	
+	
+	// Assegna o rimuove il grado di Tuto di dipartimento ad uno studente
+		protected void doGet(HttpServletRequest request, HttpServletResponse response)
+				throws ServletException, IOException {
+
+			String email = request.getParameter("email");
+			if (email == null)
+				request.setAttribute("messaggio", "Riassegnamento fallito");
+			else {
+
+				String corso = request.getParameter("corso");
+				if (corso == null)
+					request.setAttribute("messaggio", "Errore, campo corso non compilato");
+				else {
+						//cambia il tutor del corso 
+					if (userDAO.doChangeCorso(email, corso) == 0) 
+						request.setAttribute("messaggio", "Riassegnamento fallito");
+					else
+						request.setAttribute("messaggio", "Riassegnamento avvenuto");
+				}
+			}
+			RequestDispatcher view = request.getRequestDispatcher("WEB-INF/Homepage.jsp");
+			view.forward(request, response);
+
+		}
+
+		protected void doPost(HttpServletRequest request, HttpServletResponse response)
+				throws ServletException, IOException {
+			doGet(request, response);
+		}
+
+}
---
+++ Piattaforma/GestionePrenotazioni/src/Controller/NavEditTutor.java
@@ -0,0 +1,44 @@
+package Controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Model.Corso;
+import Model.CorsoDAO;
+
+/**
+ * Servlet implementation class NavEditTutor
+ */
+@WebServlet("/NavEditTutor")
+public class NavEditTutor extends ServletBasic implements Servlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		//ritorna la lista di tutti i corsi di ateneo
+		ArrayList<Corso> listaCorsi =  courseDAO.doRetrieveAll(); 
+		String email = request.getParameter("email");
+
+		request.setAttribute("listaCorsi", listaCorsi);
+		request.setAttribute("email", email);
+
+		RequestDispatcher view = request.getRequestDispatcher("WEB-INF/EditAmministratore.jsp");
+		view.forward(request, response);
+	}
+	
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
